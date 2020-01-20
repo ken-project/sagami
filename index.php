362db67a7242b907301847487ab3d0626e56a0d5
Shin50 <59241527+Shin50@users.noreply.github.com>
2020-01-20 13:26:37 +0900

    Update index.php

--- index.php
+++ index.php
@@ -239,8 +239,8 @@
     <div class="ad-wrapper">
       <img src="img/ad.png" usemap="#image-map">
       <map name="image-map">
-      <area target="" alt="tel" title="tel" href="href=&quot;tel:000-000-0000&quot;" coords="592,371,52,289" shape="rect">
-      <area target="" alt="mail" title="mail" href="href=&quot;mailto:info&amp;#64;example.com&quot;" coords="621,527,44,452" shape="rect">
+      <a href="tel:0462065157"><area target="" alt="tel" title="tel" coords="592,371,52,289" shape="rect"></a>
+                <a href="#"><area target="" alt="mail" title="mail" coords="621,527,44,452" shape="rect"></a>
   </map>
     </div>
     <!-- ad-wrapper-->
